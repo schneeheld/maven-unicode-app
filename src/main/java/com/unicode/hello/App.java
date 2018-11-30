29f18134f81af6662a5d06721226debb8cb206ae
Schnee Held <schneeheld@mail.ru>
2018-11-29 20:55:04 -0800

    Update App.java
    missing comma poll test

--- src/main/java/com/unicode/hello/App.java
+++ src/main/java/com/unicode/hello/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World" );
-        Syetem.out.println( "你好，世界" )
+        Syetem.out.println( "你好，世界" );
     }
 }
