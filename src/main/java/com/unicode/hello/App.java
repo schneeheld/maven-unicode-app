471c54b3a64c26e616c8b0088f15e3090d399308
Schnee Held <schneeheld@mail.ru>
2018-11-29 20:51:01 -0800

    hello world Unicode Chinese

--- src/main/java/com/unicode/hello/App.java
+++ src/main/java/com/unicode/hello/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World" );
+        Syetem.out.println( "你好，世界" )
     }
 }
